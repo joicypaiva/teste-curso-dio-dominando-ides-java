120bba5397a80470c36edf16d1c12b6491a80e9d
joicypaiva <joicypaiva162@gmail.com>
2022-02-24 15:29:42 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato ();
+		Livros livros = new Livros();
+		
+		System.out.println(livros);
+		System.out.println(gato);
+		}
+}
+
+
+ class Livros {
+	private String nome;
+	private String nupag;
+}
+	
+		
+	
+
+
+
